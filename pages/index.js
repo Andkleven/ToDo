6a53d791b9bb29960534b0beddc4ed070486c779
Andkleven <40411259+Andkleven@users.noreply.github.com>
2021-05-22 16:50:30 +0200

    index emty

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,3 @@
-import { useEffect } from "react";
-import { useAuth } from "../context/auth.tsx";
-
 export default function Home() {
-  const { loadingUser } = useAuth();
-
-  useEffect(() => {
-    if (loadingUser) {
-    }
-  }, [loadingUser]);
-
   return <></>;
 }
