ace28b58a08d732a4056d4b707197f608288fec9
Andkleven <40411259+Andkleven@users.noreply.github.com>
2021-06-02 21:23:26 +0200

    div

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,6 @@ import { useEffect } from "react";
 import { useAuth } from "../context/auth.tsx";
 
 export default function Home() {
-  const { loadingUser } = useAuth();
-
-  useEffect(() => {
-    if (loadingUser) {
-    }
-  }, [loadingUser]);
 
   return <></>;
 }
